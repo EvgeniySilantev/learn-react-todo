6ccacfa3f16c710aa3d54d5739ed9ad3ce93897a
Evgeniy <silantev-zhenya@mail.ru>
2019-12-21 18:30:43 +0400

    Почистил файлы, подключил bootstrap

---
+++ src/app-header/app-header.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+
+import './app-header.css'
+
+class AppHeader extends Component{
+
+    render() {
+        return (
+            <div className="app-header">
+                <h1>Todo List</h1>
+            </div>
+        );
+    }
+
+}
+
+export default AppHeader
